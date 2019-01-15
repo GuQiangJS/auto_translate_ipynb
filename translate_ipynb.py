8029f1808dd26c0c782da9d5c370fed529bcd7d6
Gu Qiang <GuQiangJS@users.noreply.github.com>
2019-01-15 09:04:16 +0800

    update translate_ipynb

--- translate_ipynb.py
+++ translate_ipynb.py
@@ -1,3 +1,21 @@
+"""
+自动转换 英文 内容的 ipynb 文档为 简体中文 文档。
+
+本工具通过调用 `py_translator` 来自动访问 Google翻译，翻译ipynb文档中的 *标记* 部分。
+
+转换后的简体中文部分会增加至原本的英文说明部分的下方，便于对照。
+
+默认文档编码为 `utf-8`
+
+转换成功后会在待转换的文件的文件夹下生成新的文件，文件名后面自动增加 `_zh-cn`。
+
+默认会使用 本机1080端口的代理 来访问Google。
+
+---
+
+如果需要转换为其他语言。可以修改文档中的 `dest` 属性设置。
+"""
+
 from __future__ import unicode_literals
 
 import json
@@ -60,19 +78,33 @@ def trans(translator, str):
     return '\n' + r
 
 
-def newTranslator():
-    return Translator(proxies={'http': '127.0.0.1:1080', 'https': '127.0.0.1:1080'})
+def newTranslator(
+        proxies={'http': '127.0.0.1:1080', 'https': '127.0.0.1:1080'}):
+    """
+
+    :param use_proxies: 代理
+    :return:
+    """
+    return Translator(proxies=proxies)
 
 
 def getFullPath():
     val = ''
     while not val or not os.path.exists(val):
-        val = input('input fullpath:')
+        val = input('Input fullpath:')
 
     d = os.path.dirname(val)
     f = os.path.basename(val)
     f, e = os.path.splitext(f)
     return val, d, f, e
+
+
+def find_cells(js):
+    """查找cells节点。所有内容均存放在此节点内。"""
+    if 'cells' in js:
+        return js['cells']
+    elif 'worksheets' in js:
+        return find_cells(js['worksheets'][0])
 
 
 if __name__ == '__main__':
@@ -85,14 +117,20 @@ if __name__ == '__main__':
             newpath = dirname
         with open(fullpath, encoding='utf=8') as f:
             js = json.load(f)
-            for j in js['cells']:
+            cells=find_cells(js)
+            if not cells:
+                logging.warning('Not found cells.')
+            for j in find_cells(js):
                 if j['cell_type'] == 'markdown' and j['source']:
                     j['source'] += '\n'
                     for i in range(0, len(j['source']), 1):
                         s = j['source'][i]
-                        j['source'].append(trans(translator, s) if BeautifulSoup(s,'lxml').text.strip() else s)
+                        j['source'].append(
+                            trans(translator, s) if BeautifulSoup(s,
+                                                                  'lxml').text.strip() else s)
                         logging.debug(s + ' -> ' + j['source'][-1])
-            with open(os.path.join(newpath, filename + '_' + dest + ext), mode='wt+', encoding=encoding) as nf:
+            with open(os.path.join(newpath, filename + '_' + dest + ext),
+                      mode='wt+', encoding=encoding) as nf:
                 json.dump(js, nf, ensure_ascii=False)
     else:
-        logging.error('fullpath is empty')
+        logging.error('Fullpath is empty')
